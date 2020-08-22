d90bb014638a99dc41abc4fb81e29167e4d1ce41
Louis Liao <39911137+a401120174@users.noreply.github.com>
2020-08-22 19:45:35 +0800

    範例完成

---
+++ src/i18n.js
@@ -0,0 +1,22 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import Backend from "i18next-http-backend";
+
+i18n
+   // load translation using http -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
+   // learn more: https://github.com/i18next/i18next-http-backend
+   .use(Backend)
+   .use(initReactI18next) // passes i18n down to react-i18next
+   .init({
+      backend: {
+         // for all available options read the backend's repository readme file
+         loadPath: "/locales/{{lng}}/{{ns}}.json",
+      },
+      fallbackLng: "en",
+      lng: "en",
+      interpolation: {
+         escapeValue: false, // not needed for react!!
+      },
+   });
+
+export default i18n;
