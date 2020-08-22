7b599863991218675e930fbde5662005a59e06da
Louis Liao <39911137+a401120174@users.noreply.github.com>
2020-08-22 20:20:51 +0800

    Updates

---
+++ precache-manifest.d8bf7cc19a8a6b4eee04ddd3505b27d7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc6d992bb3577dc15033bd3517db79c3",
+    "url": "/react-i18n-example/index.html"
+  },
+  {
+    "revision": "87f5e39efca942a8f3bd",
+    "url": "/react-i18n-example/static/css/main.a5350cd7.chunk.css"
+  },
+  {
+    "revision": "437e09bdf7a2c6fd2ad1",
+    "url": "/react-i18n-example/static/js/2.b0f03853.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-i18n-example/static/js/2.b0f03853.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87f5e39efca942a8f3bd",
+    "url": "/react-i18n-example/static/js/main.0074237f.chunk.js"
+  },
+  {
+    "revision": "20558edcc5499d14c65f",
+    "url": "/react-i18n-example/static/js/runtime-main.86a5809d.js"
+  },
+  {
+    "revision": "013f0b90a4cae7b8bbf13e3fd9e7dc4a",
+    "url": "/react-i18n-example/static/media/getFetch.013f0b90.cjs"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-i18n-example/static/media/logo.5d5d9eef.svg"
+  }
+]);
